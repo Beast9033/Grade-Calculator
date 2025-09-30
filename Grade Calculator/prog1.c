765a244ee64cfb0dc9f25ff8d651572615f75a23
Yug Patel <homeattheratenew@gmail.com>
2025-09-30 12:10:23 +0530

    Add files via upload

--- Grade Calculator/prog1.c
+++ Grade Calculator/prog1.c
@@ -18,22 +18,22 @@ int main()
     switch (grade)
     {
     case 'A':
-        printf("Your grade is A, Excellent work\n");
+        printf("Your grade is A, Excellent work, You are eligible for next level\n");
         break;
     case 'B':
-        printf("Your grade is B, Good job\n");
+        printf("Your grade is B, Good job, You are eligible for next level\n");
         break;
     case 'C':
-        printf("Your grade is C, Well done\n");
+        printf("Your grade is C, Well done, You are eligible for next level\n");
         break;
     case 'D':
-        printf("Your grade is D, Keep trying\n"); 
+        printf("Your grade is D, Keep trying, You are eligible for next level\n");
         break;
     case 'E':
-        printf("Your grade is E, You passed\n");
+        printf("Your grade is E, You passed, You are eligible for next level\n");
         break;
     default:
-        printf("Your grade is F, Sorry, You failed\n");
+        printf("Your grade is F, Sorry, You failed, You are not eligible for next level\n");
     }
 
     return 0;
